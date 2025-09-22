48fce3b6625fd598ab48b63a65ccf384eb1782e8
AnonimProgrammer <zephyr11ff@gmail.com>
2025-09-23 00:24:43 +0400

    Converted to record.

--- src/main/java/com/security/dto/request/AuthRequest.java
+++ src/main/java/com/security/dto/request/AuthRequest.java
@@ -2,22 +2,20 @@ package com.security.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
 
-@Data
-public class AuthRequest {
+public record AuthRequest(
 
-    @NotNull(message = "Phone number is required.")
-    @Pattern(
-            regexp = "^\\+\\d{1,3}-\\d{2}-\\d{3}-\\d{2}-\\d{2}$",
-            message = "Phone number must follow format +XXX-XX-XXX-XX-XX"
-    )
-    private String phoneNumber;
+        @NotNull(message = "Phone number is required.")
+        @Pattern(
+                regexp = "^\\+\\d{1,3}-\\d{2}-\\d{3}-\\d{2}-\\d{2}$",
+                message = "Phone number must follow format +XXX-XX-XXX-XX-XX"
+        )
+        String phoneNumber,
 
-    @NotNull(message = "PIN is required.")
-    @Pattern(
-            regexp = "\\d{6}",
-            message = "PIN must consist of exactly 6 digits."
-    )
-    private String pin;
-}
+        @NotNull(message = "PIN is required.")
+        @Pattern(
+                regexp = "\\d{6}",
+                message = "PIN must consist of exactly 6 digits."
+        )
+        String pin
+) {}
