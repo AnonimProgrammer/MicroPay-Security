152295073b386b40bef9be74ea4e574c61bd1016
AnonimProgrammer <zephyr11ff@gmail.com>
2025-09-23 00:24:14 +0400

    Converted to record.

--- src/main/java/com/security/dto/request/RegisterRequest.java
+++ src/main/java/com/security/dto/request/RegisterRequest.java
@@ -1,29 +1,28 @@
 package com.security.dto.request;
 
 import jakarta.validation.constraints.*;
-import lombok.Data;
 
-@Data
-public class RegisterRequest {
+public record RegisterRequest(
 
-    @NotNull(message = "Phone number is required.")
-    @Pattern(
-            regexp = "^\\+\\d{1,3}-\\d{2}-\\d{3}-\\d{2}-\\d{2}$",
-            message = "Phone number must follow format +XXX-XX-XXX-XX-XX"
-    )
-    private String phoneNumber;
+        @NotNull(message = "Phone number is required.")
+        @Pattern(
+                regexp = "^\\+\\d{1,3}-\\d{2}-\\d{3}-\\d{2}-\\d{2}$",
+                message = "Phone number must follow format +XXX-XX-XXX-XX-XX"
+        )
+        String phoneNumber,
 
-    @NotBlank(message = "Full name is required.")
-    @Size(min = 2, max = 50, message = "Full name must be between 2 and 50 characters.")
-    private String fullName;
+        @NotBlank(message = "Full name is required.")
+        @Size(min = 2, max = 50, message = "Full name must be between 2 and 50 characters.")
+        String fullName,
 
-    @Email(message = "Invalid email address.")
-    private String email;
+        @NotBlank(message = "Email is required.")
+        @Email(message = "Invalid email address.")
+        String email,
 
-    @NotNull(message = "PIN is required.")
-    @Pattern(
-            regexp = "\\d{6}",
-            message = "PIN must consist of exactly 6 digits."
-    )
-    private String pin;
-}
+        @NotNull(message = "PIN is required.")
+        @Pattern(
+                regexp = "\\d{6}",
+                message = "PIN must consist of exactly 6 digits."
+        )
+        String pin
+) {}
