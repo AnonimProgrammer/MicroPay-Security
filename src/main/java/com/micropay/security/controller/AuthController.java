ca6650b0663453a5080caa6281cb5e76ad12ad19
AnonimProgrammer <zephyr11ff@gmail.com>
2025-09-25 19:26:20 +0400

    POST /refresh-access-token endpoint is added.

--- src/main/java/com/micropay/security/controller/AuthController.java
+++ src/main/java/com/micropay/security/controller/AuthController.java
@@ -2,22 +2,32 @@ package com.micropay.security.controller;
 
 import com.micropay.security.dto.request.RegisterRequest;
 import com.micropay.security.dto.response.AuthResponse;
-import com.micropay.security.service.security.impl.UserAuthenticationServiceImpl;
+import com.micropay.security.service.security.UserAuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final UserAuthenticationServiceImpl userAuthenticationService   ;
+    private final UserAuthenticationService userAuthenticationService   ;
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
-        AuthResponse response = userAuthenticationService.registerUser(registerRequest);
+        AuthResponse response = userAuthenticationService
+                .registerUser(registerRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/refresh-access-token")
+    public ResponseEntity<AuthResponse> refreshAccessToken(@RequestHeader ("X-User-Id") UUID userId) {
+        AuthResponse response = userAuthenticationService
+                .refreshAccessToken(userId);
         return ResponseEntity.ok(response);
     }
 
